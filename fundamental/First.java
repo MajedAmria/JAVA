fa6372bde85c1187c082b0127d5242e51e605953
Majed Amria <majedamrya@hotmail.com>
2022-04-23 12:24:47 +0300

    first assignment of java

---
+++ fundamental/First.java
@@ -0,0 +1,8 @@
+public class First {
+        public static void main(String[] args) {
+            System.out.println("My name is Majed Amria");
+            System.out.println("I am 26 years old");
+            System.out.println("My hometown is Palestine");
+        }
+    }  
+
