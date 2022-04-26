b4c2220bb09eb7b5e158128a7580d90151a21cb9
Majed Amria <majedamrya@hotmail.com>
2022-04-26 16:30:30 +0300

    assignment of java

--- fundamental/Puzzling/PuzzleJava.java
+++ fundamental/Puzzling/PuzzleJava.java
@@ -17,6 +17,7 @@ public class PuzzleJava {
     }
     public char getRandomLetter(){
         Random random = new Random();
+      
         char[] arr={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
         return arr[random.nextInt(26)];
