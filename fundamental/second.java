c243758c53fabc01e423786cc6d8795a8bc4577f
majed <majedamrya@hotmail.com>
2022-05-07 10:16:57 +0300

    array nth

---
+++ fundamental/second.java
@@ -0,0 +1,14 @@
+public class second{
+
+
+    public void sec(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[arr.length-2]);
+        }
+
+    }
+
+    public static void main(String []args){
+        // sec({ 3, 4, 5, 6, 100, 200});
+    }
+}
---
+++ fundamental/src/nth_array/Array.java
@@ -0,0 +1,16 @@
+package nth_array;
+
+import java.util.Arrays;
+
+public class Array {
+
+	 
+	public static void main(String[] args) {
+		
+		int[] arr= {100, 4, 5, 6,3 , 200};
+		Arrays.sort(arr);
+		System.out.println(arr[arr.length-2]);
+    	
+
+}
+}
