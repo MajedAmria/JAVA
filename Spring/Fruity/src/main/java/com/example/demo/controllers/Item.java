6d5ab24d11d5b76b21af7cd3279c6b0c22229112
majed <majedamrya@hotmail.com>
2022-05-09 15:19:34 +0300

    Spring

---
+++ Spring/Fruity/src/main/java/com/example/demo/controllers/Item.java
@@ -0,0 +1,31 @@
+package com.example.demo.controllers;
+
+public class Item {
+    
+    // MEMBER VARIABLEScopy
+    private String name;
+    private double price;
+    
+    // CONSTRUCTOR
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+    
+}
